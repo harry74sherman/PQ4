7ef121ec82d2af7b86c5c57beaf128962b114624
harry74sherman <harry74sherman@gmail.com>
2020-11-22 23:34:08 -0500

    Got rid of main and added it to source code

--- training.py
+++ training.py
@@ -1,4 +1,139 @@
-"""TODO: add more here..."""
+import time
+import numpy as np
+import csv
+import datetime
+import json
+import os
+from nltk.stem.lancaster import LancasterStemmer
+import nltk
+nltk.download('punkt')
+
+
+def get_raw_training_data(filename):
+    '''creates a list of dictionaries from an input csv file'''
+
+    training_data = []
+
+    with open(filename, mode='r') as infile:
+        reader = csv.reader(infile)
+
+        author = {}
+        for person, sentence in reader:
+            author["person"] = person
+            author["sentence"] = sentence
+            training_data.append(author)
+
+    print(training_data)
+
+    return training_data
+
+
+def preprocess_words(words, stemmer):
+    '''Stem all words'''
+
+    # initialize list of stemmed words
+    stemmed_words = []
+
+    # remove punctuation
+    # TODO: Check this
+    no_punctuation = [word for word in words if word.isalpha()]
+
+    # stem tokens
+    stemmed_words = [stemmer.stem(word).lower()
+                     for word in no_punctuation]  # TODO: Check this
+
+    # remove duplicates
+    stemmed_words = set(stemmed_words)
+    stemmed_words = list(stemmed_words)
+
+    return stemmed_words
+
+
+def organize_raw_training_data(raw_training_data, stemmer):
+    '''Generates words, classes, and classes'''
+
+    # initialize list of all tokens
+    words = []
+
+    # initialize list for actors we haven't heard before (no duplicates)
+    classes = []
+
+    # initialize list that will contain tuples of actors and their tokens
+    documents = []
+
+    for data in raw_training_data:
+
+        #tokenize sentence
+        tokenized_sentence = nltk.word_tokenize(data["sentence"])
+
+        #add tokens to words
+        words.extend(tokenized_sentence)
+
+        actor = data["person"]
+
+        #add tuple  (tokens, actor) to documents
+        documents.append((tokenized_sentence, actor))
+
+        #if actor not in classes, add to classes
+        if actor not in classes:
+            classes.append(actor)
+
+    # preprocess words to only contain stems
+    stemmed_words = preprocess_words(words, stemmer)
+
+    return stemmed_words, classes, documents
+
+
+def create_training_data(stemmed_words, classes, documents, stemmer):
+    '''Creates training data'''
+
+    # initialize training data (list I think?)
+    training_data = []
+
+    # initialize output
+    output = []
+
+    # fill training_data_variable
+    print(type(documents))
+    for doc in documents:
+        sentence = doc[0]  # index tuple for sentence
+
+        # initialize and fill the bag
+        bag_of_words = [1 if word in stemmed_words else 0 for word in sentence]
+
+        # append new bag to old bag
+        training_data.append(bag_of_words)
+
+        # initialize list of 0s of len classes
+        actor_arr = []
+        for i in range(len(classes)):
+            output.append(0)
+
+        actor = doc[1]  # index tuple for actor
+
+        # mark the actor
+        try:
+            index = classes.index(actor)  # TODO: Check this
+            output[index] = 1
+        except:
+            pass  # actor not in list
+
+        # append actor to output
+        output.append(actor_arr)
+
+    return training_data, output
+
+
+def sigmoid(z):
+    '''calculates sigmoid of z'''
+
+    return 1 / (1 + np.exp(-z))
+
+
+def sigmoid_output_to_derivative(output):
+    '''Convert the sigmoid function's output to its derivative.'''
+
+    return output * (1-output)
 
 """* * * TRAINING * * *"""
 def init_synapses(X, hidden_neurons, classes):
@@ -170,6 +305,20 @@ def classify(words, classes, sentence):
 
 def main():
     """TODO: more instructions here..."""
+
+    # initialize stemmer
+    stemmer = LancasterStemmer()
+
+    # get raw training data
+    raw_training_data = get_raw_training_data('dialogue_data.csv')
+
+    # organize data and get words, classes, and documents
+    words, classes, documents = organize_raw_training_data(
+        raw_training_data, stemmer)
+
+    # create training data and output
+    training_data, output = create_training_data(
+        words, classes, documents, stemmer)
 
     # Comment this out if you have already trained once and don't want to re-train.
     start_training(words, classes, training_data, output)
